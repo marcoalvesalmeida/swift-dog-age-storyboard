98453cc8bb3fbb1d4e789dec4060aa2f191ed1a9
Marco Almeida <marco.ifnmg@gmail.com>
2023-01-10 16:19:23 -0300

    feat: create interface

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DogAgeStoryboard
+//
+//  Created by Marco Almeida on 10/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func getDogAge(_ sender: UIButton) {
+        print("Calculando idade do doguinho")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
